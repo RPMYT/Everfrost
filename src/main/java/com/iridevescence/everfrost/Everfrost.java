e26b3ecc39712ea7ec8fca68fe6939f04e3f4288
Prismatic <prismatic@lilydev.net>
2022-11-12 21:33:54 +1300

    Initial commit

---
+++ src/main/java/com/iridevescence/everfrost/Everfrost.java
@@ -0,0 +1,10 @@
+package com.iridevescence.everfrost;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Everfrost implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/iridevescence/everfrost/client/EverfrostClient.java
@@ -0,0 +1,13 @@
+package com.iridevescence.everfrost.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class EverfrostClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
